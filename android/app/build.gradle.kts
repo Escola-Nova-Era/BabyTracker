d27f76fc0a0dd955d19c68cf54a728364e48b700
Dierlisson Justiniano <dierlisson1@gmail.com>
2026-04-13 21:54:17 -0300

    build: adiciona dependencias e plugins do Jetpack Compose

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     alias(libs.plugins.android.application)
+    id("org.jetbrains.kotlin.plugin.compose") version "2.3.20" // esta versao ja gerencia a compatibilidade do compilador automaticamente com a versao do Kotlin.
 }
 
 android {
@@ -27,6 +28,10 @@ android {
             )
         }
     }
+    buildFeatures {
+        // Ativando o Compose no projeto.
+        compose = true
+    }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
@@ -39,6 +44,22 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+
+    // Gerenciamento de versões via BoM (Bill of Materials)
+    val composeBom = platform("androidx.compose:compose-bom:2026.03.01")
+    implementation(composeBom)
+    androidTestImplementation(composeBom)
+
+    // Dependências principais do Compose (As bibliotecas abaixo usarão as versões definidas pelo BoM automaticamente)
+    implementation(libs.androidx.ui)
+    implementation(libs.androidx.material3)
+    implementation(libs.androidx.ui.tooling.preview)
+    debugImplementation(libs.androidx.ui.tooling)
+
+    // Integração com Activity e Lifecycle
+    implementation(libs.androidx.activity.compose)
+    implementation(libs.androidx.lifecycle.viewmodel.compose)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
