d0346463b5fd6c6802efceced9716825607eaa77
Rick Nunes Souza <henriquenunes0510@icloud.com>
2026-04-23 13:44:11 -0700

    Remove duplicate bottom navigation

--- android/app/src/main/java/com/escolanovaeratech/babytracker/MainActivity.kt
+++ android/app/src/main/java/com/escolanovaeratech/babytracker/MainActivity.kt
@@ -1,13 +1,10 @@
 package com.escolanovaeratech.babytracker
 
+import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.ui.platform.ComposeView
-import com.escolanovaeratech.babytracker.ui.components.BabyTrackerApp // Importe sua função
-import com.escolanovaeratech.babytracker.ui.theme.BabyTrackerTheme
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,20 +12,10 @@ class MainActivity : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
-        // Configuração de Insets para não sobrepor as barras do sistema
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { view, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            view.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-
-        // Injetando o Compose na View XML
-        val composeView = findViewById<ComposeView>(R.id.compose_view)
-        composeView.setContent {
-            BabyTrackerTheme {
-                // Chamando sua lógica de navegação adaptativa
-                BabyTrackerApp()
-            }
         }
     }
 }
