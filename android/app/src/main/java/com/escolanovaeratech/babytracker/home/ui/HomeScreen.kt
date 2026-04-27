e49eedd9b64d3686a0bc09cad00a7b0c469aeb28
Fabio Madeira <43655104+fabiocmad@users.noreply.github.com>
2026-04-27 19:52:45 +0100

    Update HomeScreen.kt
    Update HomeScreen.kt

--- android/app/src/main/java/com/escolanovaeratech/babytracker/home/ui/HomeScreen.kt
+++ android/app/src/main/java/com/escolanovaeratech/babytracker/home/ui/HomeScreen.kt
@@ -7,7 +7,7 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.escolanovaeratech.babytracker.R
 
-class TelaHomePrincipal : AppCompatActivity() {
+class HomeScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
