e649679be23fd2f89a26f2332e2300cb680ecf6e
Madu Oliveira <sdomadu@gmail.com>
2026-05-14 18:47:19 +0200

    Fix package declaration in ProfileScreen.kt

--- android/app/src/main/java/com/escolanovaeratech/babytracker/profile/ui/ProfileScreen.kt
+++ android/app/src/main/java/com/escolanovaeratech/babytracker/profile/ui/ProfileScreen.kt
@@ -1,4 +1,4 @@
-package com.example.componentetelaperfil.ui
+package package com.escolanovaeratech.babytracker.profile.ui
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
