31697cf4aaf5d3292a24f0747c47ae7d41ec5fd9
Talles Guerra <talles-guerra@hotmail.com>
2026-05-18 19:24:41 +0100

    refactor: rename theme and fix component naming typos

--- android/app/src/main/java/com/escolanovaeratech/babytracker/ui/components/BarraInferior.kt
+++ android/app/src/main/java/com/escolanovaeratech/babytracker/ui/components/BarraInferior.kt
@@ -13,13 +13,11 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavHostController
-import androidx.navigation.compose.rememberNavController
 
 
 @Composable
-fun BarrarInferior(
+fun BarraInferior(
     navController: NavHostController,
     modifier: Modifier = Modifier
 ) {
