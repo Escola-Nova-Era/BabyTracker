e099304ccfa50334cd026bcf83e4450341e6c0c4
Ismael Costa <ismaelirc@gmail.com>
2026-09-03 15:39:22 -0300

    feat(ios): cria estrutura e novos arquivos para a tela de onboarding

---
+++ ios/BabyTracker/Screens/Onboarding/Components/OnboardingPageView.swift
@@ -0,0 +1,23 @@
+//
+//  OnboardingPageView.swift
+//  BabyTracker
+//
+//  Created by Ismael Costa on 03/09/26.
+//
+
+import SwiftUI
+
+struct OnboardingPageView: View {
+    let page: OnboardingPage
+    
+    
+    var body: some View {
+        VStack{
+            
+        }
+    }
+}
+
+#Preview {
+    OnboardingPageView()
+}
