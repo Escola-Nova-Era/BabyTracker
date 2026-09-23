1521f7a9087be16818d6689004ddade399a2df24
Ismael Costa <ismaelirc@gmail.com>
2026-09-22 22:48:04 -0300

    feat(ios) Adicionando página de formulário após onboarding, somente layout sem navegação

---
+++ ios/BabyTracker/Screens/Onboarding/OnboardingFormView.swift
@@ -0,0 +1,18 @@
+//
+//  OnboardingFormView.swift
+//  BabyTracker
+//
+//  Created by Ismael Costa on 15/09/26.
+//
+
+import SwiftUI
+
+struct OnboardingFormView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OnboardingFormView()
+}
